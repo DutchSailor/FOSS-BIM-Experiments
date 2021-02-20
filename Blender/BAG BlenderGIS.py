7ed5ef80a785189da645eec3d9733d7786d0f321
Maarten Vroegindeweij <30430941+DutchSailor@users.noreply.github.com>
2021-02-20 22:42:56 +0100

    Blender GIS Annotation

--- Blender/BAG BlenderGIS.py
+++ Blender/BAG BlenderGIS.py
@@ -83,6 +83,7 @@ def GIS2BIM_DataFromWFS(serverName,boundingBoxString,xPathStringCoord,xPathStrin
 
 ## GIS2BIM within BLENDER
 
+
 import urllib.request
 import urllib
 import xml.etree.ElementTree as ET
@@ -99,37 +100,57 @@ Rdx = crsx
 Rdy = crsy
 width = 500
 height = 500
+scalefactor = 1.6
 
 Bbox = GIS2BIM_CreateBoundingBox(Rdx,Rdy,width,height,2)
 
-curves = GIS2BIM_PointsFromWFS(DutchGEOBAG,Bbox,xPathCadastre1,-Rdx,-Rdy,1.6,3,2)
+curves = GIS2BIM_PointsFromWFS(DutchGEOBAG,Bbox,xPathCadastre1,-Rdx,-Rdy,scalefactor,3,2)
 
-curvesWFS = []
-for i in curves:
-    verts = []
-    for j in i:
-        verts.append((j[0], j[1], 0))
-    curvesWFS.append(verts)
+def GIS2BIM_BLENDER_CurvestoBlenderCurves(curves):
+    blenderCurves = []
+    for i in curves:
+        verts = []
+        for j in i:
+            verts.append((j[0], j[1], 0))
+        blenderCurves.append(verts)
+    return blenderCurves
 
-def add_mesh(name, verts, faces, edges=None, col_name="Collection"):    
+def GIS2BIM_BLENDER_add_mesh(name, verts, faces, edges=None, col_name="Collection"):    
     if edges is None:
         edges = []
     mesh = bpy.data.meshes.new(name) #mesh bouwen/vlak
     obj = bpy.data.objects.new(mesh.name, mesh) #create object
     col = bpy.data.collections.get(col_name) #Defineren/krijgen van de col
-    
     col.objects.link(obj)
     bpy.context.view_layer.objects.active = obj
-    mesh.from_pydata(verts, edges, faces)
+    mesh = mesh.from_pydata(verts, edges, faces)
+    return mesh
 
-a = 0
-for i in curvesWFS:
-    a = a + 1 
-    firstItem = i[0]
-    i.append(firstItem) # closed polygon
-    vlength = len(i)
-    result = list(range(vlength))
-    faces = [result]
-    add_mesh("BAG" + str(a), i, faces)
-    
-   
+def GIS2BIM_BLENDER_CurvesToMesh(BlenderCurves,Prefix):
+    a = 0
+    for i in BlenderCurves:
+        a = a + 1 
+        firstItem = i[0]
+        i.append(firstItem) # closed polygon
+        vlength = len(i)
+        result = list(range(vlength))
+        faces = [result]
+        GIS2BIM_BLENDER_add_mesh(Prefix + str(a), i, faces)
+    return faces
+
+def GIS2BIM_BLENDER_PlaceText(textData,fontSize):
+    for i, j, k in zip(textData[0], textData[1], textData[2]):
+        loc_txt = bpy.data.curves.new(type="FONT",name="txt") 
+        loc_txt.body = k
+        loc_obj = bpy.data.objects.new("GIS2BIM-text", loc_txt)
+        loc_obj.location = (i[0][0], i[0][1], 0)
+        bpy.context.scene.collection.objects.link(loc_obj)
+    return loc_obj   
+
+BAGCurves = GIS2BIM_BLENDER_CurvesToMesh(GIS2BIM_BLENDER_CurvestoBlenderCurves(curves),'BAG')
+
+textDataCadastralParcels = GIS2BIM_DataFromWFS(DutchGEOCadastreServerRequest2,Bbox,xPathCadastre2,xPathStringsCadastreTextAngle,-Rdx,-Rdy,scalefactor,3,2)
+textDataOpenbareRuimtenaam = GIS2BIM_DataFromWFS(DutchGEOCadastreServerRequest3,Bbox,xPathCadastre2,xPathStringsCadastreTextAngle,-Rdx,-Rdy,scalefactor,3,2)
+
+placeBlenderTextCadastralParcels = GIS2BIM_BLENDER_PlaceText(textDataCadastralParcels,50)
+placeBlenderTextOpenbareRuimtenaam = GIS2BIM_BLENDER_PlaceText(textDataOpenbareRuimtenaam,50)
