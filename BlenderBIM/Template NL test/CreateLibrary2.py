8029ce696c749e51616bdcfa07f6756e95753d7e
Maarten Vroegindeweij <30430941+DutchSailor@users.noreply.github.com>
2023-08-16 08:17:35 +0200

    Update

--- BlenderBIM/Template NL test/CreateLibrary2.py
+++ BlenderBIM/Template NL test/CreateLibrary2.py
@@ -16,10 +16,28 @@
 # You should have received a copy of the GNU General Public License
 # along with BlenderBIM Add-on.  If not, see <http://www.gnu.org/licenses/>.
 
+# 2023-08-10: Modified by Maarten Vroegindeweij
+# documentation:
+#  https://blenderbim.org/docs-python/autoapi/ifcopenshell/api/material/add_material_set/index.html
+#  https://wiki.osarch.org/index.php?title=IFC_-_Industry_Foundation_Classes/IFC_materials
+#  https://academy.ifcopenshell.org/
 import bpy
 import ifcopenshell
 import ifcopenshell.api
 import blenderbim.tool as tool
+import sys
+import pandas as pd
+import numpy
+import pandas_ods_reader
+from pandas_ods_reader import read_ods
+
+# for grids
+import blenderbim.core.spatial
+import blenderbim.tool as tool
+from bpy.types import Operator
+from bpy.props import FloatProperty, IntProperty
+from mathutils import Vector
+from blenderbim.bim.ifc import IfcStore
 
 
 class LibraryGenerator:
@@ -29,15 +47,17 @@ class LibraryGenerator:
 
         self.file = ifcopenshell.api.run("project.create_file")
         self.project = ifcopenshell.api.run(
-            "root.create_entity", self.file, ifc_class="IfcProject", name="BlenderBIM NL Library"
+            "root.create_entity", self.file, ifc_class="IfcProject", name="BlenderBIM NL ProjectTemplate 0.1"
         )
         self.library = ifcopenshell.api.run(
-            "root.create_entity", self.file, ifc_class="IfcProjectLibrary", name="BlenderBIM NL Library POC"
+            "root.create_entity", self.file, ifc_class="IfcProjectLibrary", name="BlenderBIM NL Library 0.1"
         )
         ifcopenshell.api.run(
             "project.assign_declaration", self.file, definition=self.library, relating_context=self.project
         )
-        ifcopenshell.api.run("unit.assign_unit", self.file, length={"is_metric": True, "raw": "METERS"})
+
+        ifcopenshell.api.run("unit.assign_unit", self.file,
+                             length={"is_metric": True, "raw": "MILLIMETERS"})  # METERS OR MILLIMETERS
         model = ifcopenshell.api.run("context.add_context", self.file, context_type="Model")
         plan = ifcopenshell.api.run("context.add_context", self.file, context_type="Plan")
         self.representations = {
@@ -67,85 +87,113 @@ class LibraryGenerator:
             ),
         }
 
-        self.material = ifcopenshell.api.run("material.add_material", self.file, name="Unknown")
+        # 0 ORGANISATION DETAILS
+        organisation = self.file.createIfcOrganization()
+        organisation.Name = "BlenderBIM NL I.O."
+
+        person = self.file.createIfcPerson()
+        person.FamilyName = "M.D. Vroegindeweij"
+
+        # 0 MATERIALS
+
+        # 0.1 MATERIAL PROFILE SETS
         
+        path = "C:/Users/mikev/Documents/GitHub/FOSS-BIM-Experiments/BlenderBIM/Template NL test/Library.ods"
+        library_path = "C:/BlenderBIM/3bm/Standards test folder/IFC4 NL Demo Template 0.1.ifc"
+        sheet_name = "materials"
+        library_mat = read_ods(path, sheet_name)
 
-        self.create_layer_type("IfcWallType", "WAL50", 0.05)
-        self.create_layer_type("IfcWallType", "WAL100", 0.1)
-        self.create_layer_type("IfcWallType", "WAL200", 0.2)
-        self.create_layer_type("IfcWallType", "WAL300", 0.3)
+        for ind in library_mat.index:
+            material_name = library_mat["IfcElementType"][ind]
+            style_name = library_mat["IfcElementType"][ind] + "_S"
+            material_category = library_mat["Category"][ind]
+            rgb = library_mat["RGB"][ind]
+            red = float(rgb.split(',')[0]) / 51
+            green = float(rgb.split(',')[1]) / 51
+            blue = float(rgb.split(',')[2]) / 51
 
-        self.create_layer_type("IfcCoveringType", "COV10", 0.01)
+            material_obj = globals()[material_name] = ifcopenshell.api.run("material.add_material", self.file,
+                                                                           name=material_name,
+                                                                           category=material_category)
 
-        product = self.create_layer_type("IfcCoveringType", "COV20", 0.02)
-        pset = ifcopenshell.api.run("pset.add_pset", self.file, product=product, name="EPset_Parametric")
-        ifcopenshell.api.run("pset.edit_pset", self.file, pset=pset, properties={"LayerSetDirection": "AXIS2"})
+            context = self.file.createIfcGeometricRepresentationContext()
+            style = ifcopenshell.api.run("style.add_style", self.file, name=style_name)
 
-        product = self.create_layer_type("IfcCoveringType", "COV30", 0.03)
-        pset = ifcopenshell.api.run("pset.add_pset", self.file, product=product, name="EPset_Parametric")
-        ifcopenshell.api.run("pset.edit_pset", self.file, pset=pset, properties={"LayerSetDirection": "AXIS3"})
+            ifcopenshell.api.run("style.add_surface_style", self.file,
+                                 style=style, ifc_class="IfcSurfaceStyleShading", attributes={
+                    "SurfaceColour": {"Name": style_name, "Red": red, "Green": green, "Blue": blue},
+                    "Transparency": 0.,  # 0 is opaque, 1 is transparent
+                })
+            ifcopenshell.api.run(
+                "style.assign_material_style",
+                self.file,
+                material=material_obj,
+                style=style,
+                context=context,
+            )
 
-        self.create_layer_type("IfcRampType", "RAM200", 0.2)
+        # 1 WALL TYPES SINGLE LAYER
+        sheet_name = "walls_single_layer"
+        library_walls = read_ods(path, sheet_name)
 
-        profile = self.file.create_entity("IfcCircleProfileDef", ProfileType="AREA", Radius=0.3)
-        self.create_profile_type("IfcPileType", "P1", profile)
+        for ind in library_walls.index:
+            wall_name = library_walls["Name"][ind]
+            wall_thickness = float(library_walls["Thickness"][ind])
+            wall_description = library_walls["Description"][ind]
+            wall_material = library_walls["Material"][ind]
+            wall_material_var = globals()[wall_material]
+            self.create_layer_type("IfcWallType", wall_name, wall_description, wall_thickness,
+                                   wall_material_var)  # create wall type
 
-        self.create_layer_type("IfcSlabType", "FLR150", 0.2)
-        self.create_layer_type("IfcSlabType", "FLR250", 0.3)
+        # 2 FLOOR TYPES SINGLE LAYER
+        sheet_name = "floors_single_layer"
+        library_floors = read_ods(path, sheet_name)
 
-        profile = self.file.create_entity(
-            "IfcRectangleProfileDef", ProfileName="500x600", ProfileType="AREA", XDim=0.5, YDim=0.6
-        )
-        self.create_profile_type("IfcColumnType", "C1", profile)
+        for ind in library_floors.index:
+            floor_name = library_floors["Name"][ind]
+            floor_thickness = float(library_floors["Thickness"][ind])
+            floor_description = library_floors["Description"][ind]
+            floor_material = library_floors["Material"][ind]
+            floor_material_var = globals()[floor_material]
+            self.create_layer_type("IfcSlabType", floor_name, floor_description, floor_thickness,
+                                   floor_material_var)  # create floor type
 
-        profile = self.file.create_entity(
-            "IfcCircleHollowProfileDef",
-            ProfileName="500.0x5.0 CHS",
-            ProfileType="AREA",
-            Radius=0.25,
-            WallThickness=0.005,
-        )
-        self.create_profile_type("IfcColumnType", "C2", profile)
+        # 3 RECTANGLE PROFILES, BEAMS, COLUMNS
+        sheet_name = "profiles_rect"
+        library_rectprofile = read_ods(path, sheet_name)
 
-        profile = self.file.create_entity(
-            "IfcRectangleHollowProfileDef",
-            ProfileName="150x75x2.0 RHS",
-            ProfileType="AREA",
-            XDim=0.075,
-            YDim=0.15,
-            WallThickness=0.002,
-            InnerFilletRadius=0.005,
-            OuterFilletRadius=0.005,
-        )
-        self.create_profile_type("IfcColumnType", "C3", profile)
+        for ind in library_rectprofile.index:
+            profile_name = library_rectprofile["Name"][ind]
+            profile_width = float(library_rectprofile["Width"][ind])
+            profile_height = float(library_rectprofile["Height"][ind])
+            profile_material = library_rectprofile["Material"][ind]
+            profile_material_var = globals()[profile_material]
+            make_column = library_rectprofile["MakeIfcColumnType"][ind]
+            make_beam = library_rectprofile["MakeIfcBeamType"][ind]
+            profile = self.file.create_entity("IfcRectangleProfileDef", ProfileName=profile_name, ProfileType="AREA",
+                                              XDim=profile_width, YDim=profile_height)  # profile
+            if make_column is True:
+                self.create_profile_type("IfcColumnType", profile_name, profile, profile_material_var)
+            if make_beam is True:
+                self.create_profile_type("IfcBeamType", profile_name, profile, profile_material_var)
 
-        profile = self.file.create_entity(
-            "IfcIShapeProfileDef",
-            ProfileName="DEMO-I",
-            ProfileType="AREA",
-            OverallWidth=0.1,
-            OverallDepth=0.2,
-            WebThickness=0.005,
-            FlangeThickness=0.01,
-            FilletRadius=0.005,
-        )
-        self.create_profile_type("IfcBeamType", "B1", profile)
+        # 4 ROUND PROFILES, BEAMS, COLUMNS
+        sheet_name = "profiles_round"
+        library_roundprofile = read_ods(path, sheet_name)
 
-        profile = self.file.create_entity(
-            "IfcCShapeProfileDef",
-            ProfileName="DEMO-C",
-            ProfileType="AREA",
-            Depth=0.2,
-            Width=0.1,
-            WallThickness=0.0015,
-            Girth=0.03,
-            InternalFilletRadius=0.005,
-        )
-        self.create_profile_type("IfcBeamType", "B2", profile)
-
-        #self.create_type("IfcWindowType", "WT01", {"model_body": "Window", "plan_body": "Window-Plan"})
-        #self.create_type("IfcDoorType", "DT01", {"model_body": "Door", "plan_body": "Door-Plan"})
-        #self.create_type("IfcFurnitureType", "BUN01", {"model_body": "Bunny", "plan_body": "Bunny-Plan"})
+        for ind in library_roundprofile.index:
+            profile_name = library_roundprofile["Name"][ind]
+            profile_radius = float(library_roundprofile["Radius"][ind])
+            profile_material = library_roundprofile["Material"][ind]
+            profile_material_var = globals()[profile_material]
+            make_column = library_roundprofile["MakeIfcColumnType"][ind]
+            make_beam = library_roundprofile["MakeIfcBeamType"][ind]
+            profile = self.file.create_entity("IfcCircleProfileDef", ProfileName=profile_name, ProfileType="AREA",
+                                              Radius=profile_width)  # profile
+            if make_column is True:
+                self.create_profile_type("IfcColumnType", profile_name, profile, profile_material_var)
+            if make_beam is True:
+                self.create_profile_type("IfcBeamType", profile_name, profile, profile_material_var)
 
         self.create_line_type("DASHED", "dashed")
         self.create_line_type("FINE", "fine")
@@ -155,12 +203,13 @@ class LibraryGenerator:
         self.create_line_type("STRONG", "strong")
         self.create_text_type("DOOR-TAG", "door-tag", ["{{type.Name}}", "{{Name}}"])
         self.create_text_type("WINDOW-TAG", "window-tag", ["{{Name}}"])
-        self.create_text_type("SPACE-TAG", "space-tag", ["{{Name}}", "{{Description}}", "``round({{Qto_SpaceBaseQuantities.NetFloorArea}} or 0., 2)``"])
+        self.create_text_type("SPACE-TAG", "space-tag", ["{{Name}}", "{{Description}}",
+                                                         "``round({{Qto_SpaceBaseQuantities.NetFloorArea}} or 0., 2)``"])
         self.create_text_type("MATERIAL-TAG", "rectangle-tag", ["{{material.Name}}"])
         self.create_text_type("TYPE-TAG", "capsule-tag", ["{{type.Name}}"])
         self.create_text_type("NAME-TAG", "capsule-tag", ["{{Name}}"])
 
-        self.file.write("IFC4 NL Demo Library.ifc")
+        self.file.write(library_path)
 
     def create_line_type(self, name, classes):
         element = ifcopenshell.api.run("root.create_entity", self.file, ifc_class="IfcTypeProduct", name=name)
@@ -194,21 +243,22 @@ class LibraryGenerator:
             "geometry.assign_representation", self.file, product=element, representation=representation
         )
 
-    def create_layer_type(self, ifc_class, name, thickness):
-        element = ifcopenshell.api.run("root.create_entity", self.file, ifc_class=ifc_class, name=name)
+    def create_layer_type(self, ifc_class, name, description, thickness, material_element):
+        element = ifcopenshell.api.run("root.create_entity", self.file, ifc_class=ifc_class, name=name,
+                                       description=description)
         rel = ifcopenshell.api.run("material.assign_material", self.file, product=element, type="IfcMaterialLayerSet")
         layer_set = rel.RelatingMaterial
-        layer = ifcopenshell.api.run("material.add_layer", self.file, layer_set=layer_set, material=self.material)
+        layer = ifcopenshell.api.run("material.add_layer", self.file, layer_set=layer_set, material=material_element)
         layer.LayerThickness = thickness
         ifcopenshell.api.run("project.assign_declaration", self.file, definition=element, relating_context=self.library)
         return element
 
-    def create_profile_type(self, ifc_class, name, profile):
+    def create_profile_type(self, ifc_class, name, profile, material_element):
         element = ifcopenshell.api.run("root.create_entity", self.file, ifc_class=ifc_class, name=name)
         rel = ifcopenshell.api.run("material.assign_material", self.file, product=element, type="IfcMaterialProfileSet")
         profile_set = rel.RelatingMaterial
         material_profile = ifcopenshell.api.run(
-            "material.add_profile", self.file, profile_set=profile_set, material=self.material
+            "material.add_profile", self.file, profile_set=profile_set, material=material_element
         )
         ifcopenshell.api.run("material.assign_profile", self.file, material_profile=material_profile, profile=profile)
         ifcopenshell.api.run("project.assign_declaration", self.file, definition=element, relating_context=self.library)
@@ -247,3 +297,5 @@ class LibraryGenerator:
 
 
 LibraryGenerator().generate()
+
+# 2 CREATE GRID
--- IFCOpenShell/CreateDuplicateNEN47WithPatterns.py
+++
@@ -1,1 +0,0 @@
-NEN47_1_metselwerk_baksteen = <path style="fill: #cd7c61;" d="M 0 0 3 0 3 3 0 3" />
